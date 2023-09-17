85d5fcc308054ffddbf28e3e58e28d973f0edd50
Nadeem Gulam <68492875+NadeemGulam@users.noreply.github.com>
2023-09-17 21:36:11 +0530

    updated

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -41,7 +41,7 @@ const Contact = () => {
           <article className="contact__option">
             <RiMessengerLine className="contact__option-icon" />
             <h4>Message</h4>
-            <h5>egator tutorials</h5>
+            <h5>Nadeem Gulam</h5>
             <a href="https://m.me" target="_blank">
               Send a Message
             </a>
